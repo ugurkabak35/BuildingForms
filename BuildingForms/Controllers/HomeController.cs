a54fe0a99f78b2ebc2af267bdf41cae7bef5b2b8
UGUR <53657851+ugurkabak35@users.noreply.github.com>
2021-02-11 17:10:49 +0300

    Add project files.

---
+++ BuildingForms/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildingForms.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Search()
+        {
+            return View();
+        }
+    }
+}
---
+++ BuildingForms/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildingForms.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool isApproved { get; set; }
+    }
+}
